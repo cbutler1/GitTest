7eeb6fb1de2f924446ce7a160d2eff8335a4a007
Cbutler1 <collin.p.butler1@gmail.com>
2022-06-27 21:53:53 -0400

    updated notes

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 //testing git
+
+
+//ctrl ~ opens powershell
+
+// command for git ignore (dotnet new gitignore)
+
+//git init
+
+//git status
+
+//if you want to save, you need to stage then commit the files, next add the files you want to save changes to
+
+//git add . (. adds all files)
+
+//git status
+
+//git commit -m "flag to add note"
+
+//now saved on local machine so you can go back in time and see what it looked like at this point in time.
+
+//create a redundancy by adding to github by creating repository on github and pushing existing by copy paste
